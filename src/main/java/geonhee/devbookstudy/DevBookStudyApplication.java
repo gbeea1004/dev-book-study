dc1340fb0ec6a916b7ca2ac19a3ad9bf8b14f186
geony <jvckigh@gmail.com>
2023-05-07 23:51:01 +0900

    Init project

---
+++ src/main/java/geonhee/devbookstudy/DevBookStudyApplication.java
@@ -0,0 +1,13 @@
+package geonhee.devbookstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBookStudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DevBookStudyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/geonhee/devbookstudy/DevBookStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package geonhee.devbookstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBookStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
